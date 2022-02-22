187d01bb4e0916d5be10f306fea3937ab08cd336
joydeep-mukherjee <jmukherj@calpoly.edu>
2022-02-22 12:03:01 -0800

    first commit

---
+++ lab_8c/src/lab_8c/SimpleThreadExample.java
@@ -0,0 +1,14 @@
+package lab_8c;
+
+public class SimpleThreadExample {
+	  private static final int MAX_TIMES = 100;
+
+	  /**
+	   * Demonstration of the use of Threads.
+	   *
+	   * @param args command-line arguments, not used
+	   */
+	  public static void main(String[] args) {
+	    new Thread(new WordPrinter("hello", MAX_TIMES)).start();
+	  }
+	}
